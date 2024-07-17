9735e45735ba28977e6243f6acd4bce7facda360
Anthony <anthony.eccleston@live.com>
2024-07-17 17:18:35 -0400

    Add my user model

---
+++ models/user.js
@@ -0,0 +1,98 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+});
+
+const budgetGoalSchema = new Schema({
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    amount: {
+      type: Number,
+      required: true,
+      min: 0,
+    },
+    startDate: {
+      type: Date,
+      required: true,
+    },
+    endDate: {
+      type: Date,
+      required: true,
+    },
+});
+
+const expenseSchema = new Schema({
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+    },
+    amount: {
+        type: Number,
+        required: true,
+        min: 0,
+    },
+    category: {
+        type: String,
+        required: true,
+    },
+    date: {
+        type: Date,
+        required: true,
+    },
+    description: {
+        type: String,
+    },
+});
+  
+const transactionSchema = new Schema({
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+    amount: {
+      type: Number,
+      required: true,
+      min: 0,
+    },
+    type: {
+      type: String,
+      required: true,
+      enum: ['income', 'expense'],
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    date: {
+      type: Date,
+      required: true,
+    },
+    description: {
+      type: String,
+    },
+});
+
+const User = mongoose.model('User', userSchema);
+const Transaction = mongoose.model('Transaction', transactionSchema);
+const Expense = mongoose.model('Expense', expenseSchema);
+const BudgetGoal = mongoose.model('BudgetGoal', budgetGoalSchema);
+
+module.exports = { User, Transaction, Expense, BudgetGoal };
